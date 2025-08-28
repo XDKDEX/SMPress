65aef10f63ccc3d48d3ada0b67331f65c6d3e63c
fgsyjg <103232673+XDKDEX@users.noreply.github.com>
2025-08-28 20:02:41 +0800

    Add files via upload

--- index.js
+++ index.js
@@ -2,10 +2,10 @@ const http = require('http');
 const url = require('url');
 
 
-function smpress(params) {
-    const pathMapping = {}
-    class smpress {
+function smpress() {
+    class SMPress {
         constructor() {
+            this.pathMapping = {}
             this.port = 80;
             this.routes = {
                 GET: {},
@@ -14,6 +14,7 @@ function smpress(params) {
                 DELETE: {},
                 ALL: {}
             };
+
         }
         bind(port) {
             if (typeof port !== 'number' || port < 1 || port > 65565) {
@@ -23,13 +24,15 @@ function smpress(params) {
         }
         create() {
             http.createServer((request, response) => {
-                let pathname = url.parse(request.url).pathname;
-                if (pathname in pathMapping) {
+                let urlDictionary = url.parse(request.url);
+                if (urlDictionary.pathname in this.pathMapping) {
                     let req = {};
                     let res = {};
-                    req.query = function () { return query(pathname, request.url) }
-                    pathMapping[pathname](req, res);
-                    response.end()
+                    req.query = function () { return query(urlDictionary.query || '') }
+                    res.send = function (resource) { response.write(resource) } //临时
+                    res.end = function () { response.end() } //临时
+                    this.pathMapping[urlDictionary.pathname](req, res);
+                    
                 } else {
                     response.writeHead(404);
                     response.end()
@@ -37,26 +40,20 @@ function smpress(params) {
             }).listen(this.port);
         }
         get(path, func) {
-            pathMapping[path] = func
-            console.log(pathMapping);
+            this.pathMapping[path] = func
+            console.log(this.pathMapping);
         }
     }
-    return new smpress();
+    return new SMPress();
 }
-function query(rootPath, string) {
-    string = string.substr(rootPath.indexOf('/') + 2)
+function query(string) {
+    if(string == '') return null;
     let result = {};
-    if (string.indexOf("=") == -1 && string.indexOf("&") == -1) { return null }
-    if (string.indexOf("&") == -1) {
-        let tmp = string.split("=");
-        result[tmp[0]] = tmp[1]
-    } else {
-        string = string.split("&");
-        for (let i = 0; i < string.length; i++) {
-            let tmp = string[i].split("=");
-            for (let j = 0; j < tmp.length; j++) {
-                result[tmp[0]] = tmp[1]
-            }
+    string = string.split("&");
+    for (let i = 0; i < string.length; i++) {
+        let tmp = string[i].split("=");
+        for (let j = 0; j < tmp.length; j++) {
+            result[tmp[0]] = tmp[1]
         }
     }
     return result;
