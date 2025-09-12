899fa28b97d683d041afecdf5dd6dd16af7026a8
fgsyjg <103232673+XDKDEX@users.noreply.github.com>
2025-09-12 21:30:06 +0800

    Add files via upload

--- index.js
+++ index.js
@@ -7,20 +7,16 @@ function smpress() {
         constructor() {
             this.pathMapping = {}
             this.port = 80;
-            this.routes = {
-                GET: {},
-                POST: {},
-                PUT: {},
-                DELETE: {},
-                ALL: {}
-            };
+            this.routes = {GET: {},POST: {},PUT: {},DELETE: {},ALL: {}};
 
+            return this;
         }
         bind(port) {
             if (typeof port !== 'number' || port < 1 || port > 65565) {
                 throw new Error('Port must be a number between 1 and 65565');
             }
             this.port = port;
+            return this;
         }
         create() {
             http.createServer((request, response) => {
@@ -28,9 +24,12 @@ function smpress() {
                 if (urlDictionary.pathname in this.pathMapping) {
                     let req = {};
                     let res = {};
-                    req.query = function () { return query(urlDictionary.query || '') }
-                    res.send = function (resource) { response.write(resource) } //临时
-                    res.end = function () { response.end() } //临时
+                    // console.log(request);
+                    // console.log(response);
+                    req.cookie = this.parseCookie(request.headers.cookie);
+                    // req.query = function () { return query(urlDictionary.query || '') }
+                    res.send = function (resource) { response.write(resource) } // temporary solution
+                    res.end = function () { response.end() } // temporary solution
                     this.pathMapping[urlDictionary.pathname](req, res);
                     
                 } else {
@@ -38,25 +37,35 @@ function smpress() {
                     response.end()
                 }
             }).listen(this.port);
+            return this;
         }
         get(path, func) {
             this.pathMapping[path] = func
             console.log(this.pathMapping);
+            return this;
+        }
+        parseCookie(string){
+            if (!string) return {};
+            let result = {};
+            let cookieDictionary = string.split(';') // => ['a=b','b=c']
+            let invalidIndex = 0;
+            for (let index = 0; index < cookieDictionary.length; index++) {
+                if(cookieDictionary[index].indexOf('=')==0){
+                    result['__invalid_'+invalidIndex] = cookieDictionary[index].substr(1)
+                    invalidIndex++;
+                continue;
+                }
+                result[cookieDictionary[index].split("=")[0]] = cookieDictionary[index].split("=")[1]||''
+            }
+            return result;
+            /*
+                "a="    → {a: ''}        // 空值正确表示
+                "a=b"   → {a: 'b'}       // 标准键值对
+                "=c"    → {__invalid_0: 'c'} // 无效cookie的特殊处理
+            */
         }
     }
     return new SMPress();
-}
-function query(string) {
-    if(string == '') return null;
-    let result = {};
-    string = string.split("&");
-    for (let i = 0; i < string.length; i++) {
-        let tmp = string[i].split("=");
-        for (let j = 0; j < tmp.length; j++) {
-            result[tmp[0]] = tmp[1]
-        }
-    }
-    return result;
 }
 
 module.exports = {
